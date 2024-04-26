536e31a94cc61e2a53442223bedef679693434c0
Gautham Vanjre <vanjregautham@gmail.com>
2024-04-26 18:14:17 +0530

    ref: navbar sections

--- app/WorkExperience.tsx
+++ app/WorkExperience.tsx
@@ -4,12 +4,12 @@ import Timeline from "./components/Timeline";
 const WorkExperience = () => {
   return (
     <>
-      <div className="relative">
+      <div id="workExperience" className="relative">
         <h2 className="text-center font-bold text-sm text-white-600 uppercase tracking-wider mb-6">
           Work Experience and Education
         </h2>
         <p className="text-center text-2xl sm:text-3xl mb-6 font-extrabold">
-          Where Have I worked Previously and graduated from{" "}
+          My Education and Work History
         </p>
         <Timeline />
       </div>
