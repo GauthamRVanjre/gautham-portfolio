067864360032e736d7dfec0185931d0eec592491
Gautham Vanjre <vanjregautham@gmail.com>
2024-04-29 20:16:20 +0530

    feat: form installation and form validation

--- app/contactMe/page.tsx
+++ app/contactMe/page.tsx
@@ -11,7 +11,7 @@ export default function ContactMe() {
     console.log("Form submitted");
   };
   return (
-    <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
+    <div className="max-w-md mt-24 w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
       <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
         Contact Me
       </h2>
@@ -38,7 +38,7 @@ export default function ContactMe() {
           className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
           type="submit"
         >
-          Sign up &rarr;
+          Send Message
           <BottomGradient />
         </button>
       </form>
