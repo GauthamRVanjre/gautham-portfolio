de72775a04aebdfb0fa7118cd62c47c61c637498
Gautham Vanjre <vanjregautham@gmail.com>
2024-04-30 21:21:24 +0530

    feat: resend integration

---
+++ app/api/sendEmail/route.ts
@@ -0,0 +1,18 @@
+import { resend } from "@/lib/resend";
+import { EmailTemplate } from "@/lib/email-template";
+
+export async function POST() {
+  try {
+    const data = await resend.emails.send({
+      from: "Acme <onboarding@resend.dev>",
+      to: ["vanjregautham@gmail.com"],
+      subject: "Hello world",
+      react: EmailTemplate({ firstName: "John" }),
+      text: "",
+    });
+
+    return Response.json(data);
+  } catch (error) {
+    return Response.json({ error });
+  }
+}
