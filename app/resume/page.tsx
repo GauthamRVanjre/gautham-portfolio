f4bf4bdfc133725e87da198d2ff4f29f8643bc2c
Gautham Vanjre <vanjregautham@gmail.com>
2024-05-24 20:58:50 +0530

    fix: update resume section

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,4 +1,5 @@
 import ankitresume from "@/public/ankitresume.png";
+import GauthamResume from "@/public/gautham_resume.png";
 import Image from "next/image";
 import AnimatedDiv from "../components/AnimatedDiv";
 import { MdOutlineKeyboardBackspace } from "react-icons/md";
@@ -26,9 +27,9 @@ const ResumePage = () => {
             My Resume
           </p>
           <div className="p-3 dark:bg-zinc-800 bg-zinc-100 rounded-lg">
-            <Image src={ankitresume} alt="myresume" className="rounded-md" />
+            <Image src={GauthamResume} alt="myresume" className="rounded-md" />
           </div>
-          <a href={"../../gautham_resume.pdf"} target="_blank" download>
+          <a href={"../../gautham_2023_grad.pdf"} target="_blank" download>
             <Button size={"sm"} variant={"outline"} className="my-3">
               <FiDownload className="mr-2" />
               Download
