284c5889f11356ad5dc13994e8549f3dfa5cefc1
Gautham Vanjre <vanjregautham@gmail.com>
2024-05-29 11:13:07 +0530

    fix: navbar section

--- app/nav/NavBar.tsx
+++ app/nav/NavBar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import { Button } from "@/components/ui/button";
 import styles from "./navbar.module.css";
@@ -5,8 +6,12 @@ import Link from "next/link";
 import { DarkModeSwitch } from "./DarkModeSwitch";
 import { Tooltip } from "react-tooltip";
 import { FiDownload } from "react-icons/fi";
+import { usePathname } from "next/navigation";
 
 const NavBar = () => {
+  const pathName = usePathname();
+  console.log(pathName);
+
   return (
     <div
       className={`${styles.navbar} flex justify-between my-3 md:my-6 px-[1rem] sm:px-[2rem] lg:px-[9rem]`}
@@ -15,36 +20,41 @@ const NavBar = () => {
         GV
       </Link>
       <div className="flex gap-2 relative">
-        <Button
-          size={"sm"}
-          className="bg-transparent border text-black dark:text-white hover:dark:bg-white hover:dark:text-black hover:bg-black hover:text-white"
-        >
-          <Link href="#workExperience">Work Experience</Link>
-        </Button>
-        <Button
-          size={"sm"}
-          className="bg-transparent border text-black dark:text-white hover:dark:bg-white hover:dark:text-black hover:bg-black hover:text-white"
-        >
-          <Link href="#projects">Projects</Link>
-        </Button>
-        <Button
-          size={"sm"}
-          className="bg-transparent border text-black dark:text-white hover:dark:bg-white hover:dark:text-black hover:bg-black hover:text-white"
-        >
-          <Link href="#skills">Skills</Link>
-        </Button>
-        <Button
-          size={"sm"}
-          className="bg-transparent border text-black dark:text-white hover:dark:bg-white hover:dark:text-black hover:bg-black hover:text-white"
-        >
-          <Link href="#blogs">Blogs</Link>
-        </Button>
-        <Button
-          size={"sm"}
-          className="bg-transparent border text-black dark:text-white hover:dark:bg-white hover:dark:text-black hover:bg-black hover:text-white"
-        >
-          <Link href="#about">About Me</Link>
-        </Button>
+        {pathName === "/" && (
+          <div>
+            <Button
+              size={"sm"}
+              className="bg-transparent border text-black dark:text-white hover:dark:bg-white hover:dark:text-black hover:bg-black hover:text-white"
+            >
+              <Link href="#workExperience">Work Experience</Link>
+            </Button>
+            <Button
+              size={"sm"}
+              className="bg-transparent border text-black dark:text-white hover:dark:bg-white hover:dark:text-black hover:bg-black hover:text-white"
+            >
+              <Link href="#projects">Projects</Link>
+            </Button>
+            <Button
+              size={"sm"}
+              className="bg-transparent border text-black dark:text-white hover:dark:bg-white hover:dark:text-black hover:bg-black hover:text-white"
+            >
+              <Link href="#skills">Skills</Link>
+            </Button>
+            <Button
+              size={"sm"}
+              className="bg-transparent border text-black dark:text-white hover:dark:bg-white hover:dark:text-black hover:bg-black hover:text-white"
+            >
+              <Link href="#blogs">Blogs</Link>
+            </Button>
+            <Button
+              size={"sm"}
+              className="bg-transparent border text-black dark:text-white hover:dark:bg-white hover:dark:text-black hover:bg-black hover:text-white"
+            >
+              <Link href="#about">About Me</Link>
+            </Button>
+          </div>
+        )}
+
         <Button
           size={"sm"}
           className="bg-transparent border text-black dark:text-white hover:dark:bg-white hover:dark:text-black hover:bg-black hover:text-white"
@@ -58,17 +68,6 @@ const NavBar = () => {
           </a>
         </Button>
 
-        {/* <Button
-          size={"sm"}
-          className="bg-transparent border text-black dark:text-white hover:dark:bg-white hover:dark:text-black hover:bg-black hover:text-white"
-        >
-          <a
-            href="https://www.linkedin.com/in/gautham-r-vanjre-72b858228/"
-            target="_blank"
-          >
-            Say Hi!
-          </a>
-        </Button> */}
         <Button
           size={"sm"}
           className="bg-transparent border text-black dark:text-white hover:dark:bg-white hover:dark:text-black hover:bg-black hover:text-white"
