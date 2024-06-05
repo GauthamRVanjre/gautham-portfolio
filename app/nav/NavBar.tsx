abf0d1187816e889bdbf1fce870d2defc3caa7f3
Gautham Vanjre <vanjregautham@gmail.com>
2024-06-05 10:48:59 +0530

    feat: responsive navbar

--- app/nav/NavBar.tsx
+++ app/nav/NavBar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import styles from "./navbar.module.css";
 import Link from "next/link";
@@ -7,10 +7,10 @@ import { DarkModeSwitch } from "./DarkModeSwitch";
 import { Tooltip } from "react-tooltip";
 import { FiDownload } from "react-icons/fi";
 import { usePathname } from "next/navigation";
+import { FaHamburger } from "react-icons/fa";
 
 const NavBar = () => {
   const pathName = usePathname();
-  console.log(pathName);
 
   return (
     <div
@@ -19,63 +19,70 @@ const NavBar = () => {
       <Link href={"/"} className="font-extrabold font-protest text-3xl">
         GV
       </Link>
-      <div className="flex gap-2 relative">
-        {pathName === "/" && (
-          <div>
-            <Button
-              size={"sm"}
-              className="bg-transparent border text-black dark:text-white hover:dark:bg-white hover:dark:text-black hover:bg-black hover:text-white"
-            >
-              <Link href="#workExperience">Work Experience</Link>
-            </Button>
-            <Button
-              size={"sm"}
-              className="bg-transparent border text-black dark:text-white hover:dark:bg-white hover:dark:text-black hover:bg-black hover:text-white"
-            >
-              <Link href="#projects">Projects</Link>
-            </Button>
-            <Button
-              size={"sm"}
-              className="bg-transparent border text-black dark:text-white hover:dark:bg-white hover:dark:text-black hover:bg-black hover:text-white"
-            >
-              <Link href="#skills">Skills</Link>
-            </Button>
-            <Button
-              size={"sm"}
-              className="bg-transparent border text-black dark:text-white hover:dark:bg-white hover:dark:text-black hover:bg-black hover:text-white"
-            >
-              <Link href="#blogs">Blogs</Link>
-            </Button>
-            <Button
-              size={"sm"}
-              className="bg-transparent border text-black dark:text-white hover:dark:bg-white hover:dark:text-black hover:bg-black hover:text-white"
-            >
-              <Link href="#about">About Me</Link>
-            </Button>
-          </div>
-        )}
+      <div className="relative groupNav">
+        <div className={`md:flex gap-2  hidden`}>
+          {pathName === "/" && (
+            <>
+              <div className="relative group mt-1">
+                <div className=" cursor-pointer">Services</div>
+                <div className="hidden group-hover:block absolute bg-white dark:bg-gray-800 mt-2 p-2 rounded shadow-lg">
+                  <Button
+                    size={"sm"}
+                    className="bg-transparent w-full border text-black dark:text-white hover:dark:bg-white hover:dark:text-black hover:bg-black hover:text-white"
+                  >
+                    <Link href="#workExperience">Work Experience</Link>
+                  </Button>
+                  <Button
+                    size={"sm"}
+                    className="bg-transparent border w-full text-black dark:text-white hover:dark:bg-white hover:dark:text-black hover:bg-black hover:text-white"
+                  >
+                    <Link href="#projects">Projects</Link>
+                  </Button>
+                  <Button
+                    size={"sm"}
+                    className="bg-transparent w-full border text-black dark:text-white hover:dark:bg-white hover:dark:text-black hover:bg-black hover:text-white"
+                  >
+                    <Link href="#skills">Skills</Link>
+                  </Button>
+                  <Button
+                    size={"sm"}
+                    className="bg-transparent border w-full text-black dark:text-white hover:dark:bg-white hover:dark:text-black hover:bg-black hover:text-white"
+                  >
+                    <Link href="#blogs">Blogs</Link>
+                  </Button>
+                  <Button
+                    size={"sm"}
+                    className="bg-transparent border w-full text-black dark:text-white hover:dark:bg-white hover:dark:text-black hover:bg-black hover:text-white"
+                  >
+                    <Link href="#about">About Me</Link>
+                  </Button>
+                </div>
+              </div>
+            </>
+          )}
 
-        <Button
-          size={"sm"}
-          className="bg-transparent border text-black dark:text-white hover:dark:bg-white hover:dark:text-black hover:bg-black hover:text-white"
-        >
-          {/* <Link href="/resume">Resume</Link> */}
-          <a href={"../../gautham_Software_engineer.pdf"} target="_blank">
-            <Button size={"sm"} variant={"outline"} className="my-3">
-              <FiDownload className="mr-2" />
-              Resume
-            </Button>
-          </a>
-        </Button>
+          <Button
+            size={"sm"}
+            className="bg-transparent border text-black dark:text-white hover:dark:bg-white hover:dark:text-black hover:bg-black hover:text-white"
+          >
+            {/* <Link href="/resume">Resume</Link> */}
+            <a href={"../../gautham_Software_engineer.pdf"} target="_blank">
+              <Button size={"sm"} variant={"outline"} className="my-3">
+                <FiDownload className="mr-2" />
+                Resume
+              </Button>
+            </a>
+          </Button>
 
-        <Button
-          size={"sm"}
-          className="bg-transparent border text-black dark:text-white hover:dark:bg-white hover:dark:text-black hover:bg-black hover:text-white"
-        >
-          <Link href="/contactMe">Contact Me</Link>
-        </Button>
+          <Button
+            size={"sm"}
+            className="bg-transparent border text-black dark:text-white hover:dark:bg-white hover:dark:text-black hover:bg-black hover:text-white"
+          >
+            <Link href="/contactMe">Contact Me</Link>
+          </Button>
 
-        <DarkModeSwitch />
+          <DarkModeSwitch />
+        </div>
       </div>
     </div>
   );
