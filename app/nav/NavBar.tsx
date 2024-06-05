22e67a63fb3110e95d41409ec268e795b518fe97
Gautham Vanjre <vanjregautham@gmail.com>
2024-06-05 11:08:12 +0530

    fix: navbar spacing in mobile devices and hero section

--- app/nav/NavBar.tsx
+++ app/nav/NavBar.tsx
@@ -1,26 +1,24 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import { Button } from "@/components/ui/button";
 import styles from "./navbar.module.css";
 import Link from "next/link";
 import { DarkModeSwitch } from "./DarkModeSwitch";
-import { Tooltip } from "react-tooltip";
 import { FiDownload } from "react-icons/fi";
 import { usePathname } from "next/navigation";
-import { FaHamburger } from "react-icons/fa";
 
 const NavBar = () => {
   const pathName = usePathname();
 
   return (
     <div
-      className={`${styles.navbar} flex justify-between my-3 md:my-6 px-[1rem] sm:px-[2rem] lg:px-[9rem]`}
+      className={`${styles.navbar} flex justify-between md:my-6 px-2 sm:px-[2rem] lg:px-[9rem]`}
     >
       <Link href={"/"} className="font-extrabold font-protest text-3xl">
         GV
       </Link>
       <div className="relative groupNav">
-        <div className={`md:flex gap-2  flex flex-row`}>
+        <div className={`md:flex md:gap-2  flex flex-row`}>
           {pathName === "/" && (
             <>
               <div className="relative group mt-1">
