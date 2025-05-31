ea96db15a6c5455b4cb1cfdec6bc67ba60cab2bf
theeliteclub2k <vanjregautham@gmail.com>
2025-05-31 14:20:59 +0530

    update styles

--- components/ui/typewriter-effect.tsx
+++ components/ui/typewriter-effect.tsx
@@ -156,7 +156,7 @@ export const TypewriterEffectSmooth = ({
         }}
       >
         <div
-          className="text-sm sm:text-base md:text-xl lg:text:3xl xl:text-5xl font-bold"
+          className="text-base sm:text-lg md:text-3xl lg:text-5xl xl:text-6xl font-bold"
           style={{
             whiteSpace: "nowrap",
           }}
